443695f0c83fa4d218af3abdce21ee5430ae974d
Mısra <120517367+misrakara@users.noreply.github.com>
2024-01-07 13:43:34 +0300

    btree veri ekleme.c

--- DataStructuresLab/btree veri ekleme.c
+++ DataStructuresLab/btree veri ekleme.c
@@ -2,60 +2,87 @@
 #include<stdlib.h>
 #include<conio.h>
 
-//Ýkili agac olusturmak ve veri eklemek
+//Ä°kili agac olusturmak ve veri eklemek
 
-
-typedef struct node{
+struct node{
 	int data;
 	struct node *left;
 	struct node *right;
-}BTREE;
+};
 
-BTREE *new_node(int data){
-	
-	BTREE *p=(BTREE*)malloc(sizeof(BTREE));
-	p->data=data;
-	p->left=NULL;
-	p->right=NULL;
+typedef struct node *BTREE;
+
+BTREE new_node(int x){
+	BTREE p;
+	p=(BTREE)malloc(sizeof(struct node));
+	p->data=x;
+	p->left=p->right=NULL;
+
 	
 	return p;
 	
 }
 
-BTREE *insert(BTREE *root,int data){
-	if(root!=NULL){
-		if(data < root->data)
-			root->left=insert(root->left,data);
-		
+BTREE insert(BTREE root,int x){
+	if(root==NULL)
+		root=new_node(x);
+	
+	else{
+		if(x<root->data)
+			root->left=insert(root->left,x);
 		else
-			root->right=insert(root->right,data);
-		
+			root->right=insert(root->right,x);
 	}
-	else
-	root=new_node(data);
 	
 	return root;
 }
-void inorder(BTREE *root){
+void preorder(BTREE root){
+	if(root!=NULL){
+		printf("%3d ",root->data);
+		preorder(root->left);
+		preorder(root->right);
+	}
+}
+
+void inorder(BTREE root){
 	if(root!=NULL){
 		inorder(root->left);
-		printf("%d ",root->data);
+		printf("%3d",root->data);
 		postorder(root->right);
 	}
 }
+
+void postorder(BTREE root){
+	if(root!=NULL){
+		postorder(root->left);
+		postorder(root->right);
+		printf("%3d",root->data);
+	}
+}
+
 int main(){
-	BTREE *root=NULL;
+	BTREE myroot=NULL;
 	int i;
 	printf("Agaca eklenecek verileri giriniz:\n",i);
-	do{
+	scanf("%d",&i);
+	while(i!=-1){
+		myroot=insert(myroot,i);
 		scanf("%d",&i);
-		if(i!=-1)
-			root=insert(root,i);
+	}
+	
+	
+	printf("Preorder traversal of the binary tree is:\n");
+	preorder(myroot);
+	printf("\n");
+	
+	printf("Inorder traversal of the binary tree is:\n");
+	inorder(myroot);
+	printf("\n");
+	
+	printf("Postorder traversal of the binary tree is:\n");
+	postorder(myroot);
+	printf("\n");
 		
-	}while(i!=-1);
-
-	printf("Inorder traversal of the binary tree is: ");
-	inorder(root);
 	
 	return 0;
 }
